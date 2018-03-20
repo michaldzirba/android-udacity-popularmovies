c3df958c99ad793a0ea5241b8052263b84fae7a3
mdzirba@gmail.com <mdzirba@gmail.com>
2018-03-20 16:55:02 +0100

    initial

---
+++ app/src/main/java/pl/michaldzirba/popularmovies/DetailActivity.java
@@ -0,0 +1,13 @@
+package pl.michaldzirba.popularmovies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
